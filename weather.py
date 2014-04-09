f1b80b7254ed86eaaa84e8302841e9907de2bdfe
Kurt R. Prudhomme <prud0042@crk.umn.edu>
2014-04-09 14:35:26 -0500

    Added some logic
    Able to get Yahoo weather rss.

--- weather.py
+++ weather.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
 from sys import argv
-from twython import Twython
-import commands
-import logging
+from twython import Twython ## Connect to Twitter ## NEEDED ## Install with pip
+import commands ## Call system commands ## If needed
+import logging ## Log errors and success
+import feedparser ## Get an RSS feed and parse it ## NEEDED ## Install with pip
 
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
@@ -16,7 +17,17 @@ api = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 # Logging DEBUG
 logging.basicConfig(filename='umc-weather.log', level=logging.DEBUG)
 
+# Use Yahoo! weather RSS feed to get the weather
+## SEE: https://developer.yahoo.com/weather/#request ## Yahoo! API
+## SEE: http://woeid.rosselliot.co.nz/ ## WOEID lookup
+## TODO ## Use two strings to hold the URL and the WOEID info.
+location = feedparser.parse('http://weather.yahooapis.com/forecastrss?w=12782656')
+
+## Test
+print location['feed']['title']
 
 ## TODO ##
 # Need to add logic into the script to get data from some sort of RSS
 # feed to pipe that to the status. 
+
+## SEE: http://www.tuxradar.com/content/code-project-use-weather-wallpapers ##

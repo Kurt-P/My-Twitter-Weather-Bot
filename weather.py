1c17ffe6931957f365ee00ffba3d6f586157656e
Kurt R. Prudhomme <prud0042@crk.umn.edu>
2014-04-11 14:43:50 -0500

    It works
    Added the functionality it needes to get the current tempture
    and output it to the screen.

--- weather.py
+++ weather.py
@@ -5,29 +5,40 @@ from twython import Twython ## Connect to Twitter ## NEEDED ## Install with pip
 import commands ## Call system commands ## If needed
 import logging ## Log errors and success
 import feedparser ## Get an RSS feed and parse it ## NEEDED ## Install with pip
+import re ## Parse the RSS data with a regular expression
 
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
 ACCESS_KEY = ''
 ACCESS_SECRET = ''
 
-# Setup the api object
+## Setup the api object
 api = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 
-# Logging DEBUG
+## Logging DEBUG
 logging.basicConfig(filename='umc-weather.log', level=logging.DEBUG)
 
-# Use Yahoo! weather RSS feed to get the weather
+## Use Yahoo! weather RSS feed to get the weather
 ## SEE: https://developer.yahoo.com/weather/#request ## Yahoo! API
 ## SEE: http://woeid.rosselliot.co.nz/ ## WOEID lookup
-## TODO ## Use two strings to hold the URL and the WOEID info.
-location = feedparser.parse('http://weather.yahooapis.com/forecastrss?w=12782656')
+
+## Setup the RSS sting
+url = 'http://weather.yahooapis.com/forecastrss?w='
+woeid = '12782656' ## This will need to be edited to get different locations
+rss = url + woeid ## The resulting RSS URL
+logging.info(rss) ## Doesn't hurn to log it
+data = feedparser.parse(rss)
+
+## Now we parse the data from the RSS to get the current temp
+summary = data.entries[0].summary ## Get just the summary
+temp = re.split(r'\n',re.sub('<.+?>','',summary)) ## Break the summary down
+logging.info(temp)
+temp = int(re.findall('[0-9]+',temp[2])[0]) ## Get the current temp
+logging.info(temp)
+
+line = 'The current temp is %d' % temp
+logging.debug(line)
 
 ## Test
-print location['feed']['title']
-
-## TODO ##
-# Need to add logic into the script to get data from some sort of RSS
-# feed to pipe that to the status. 
-
-## SEE: http://www.tuxradar.com/content/code-project-use-weather-wallpapers ##
+#print data['feed']['title']
+#print line
